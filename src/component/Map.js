c76ef7ad0dcea02a12303463634a9139887e3ee3
Sulaimanma <379043678@qq.com>
2021-04-28 13:23:17 +1000

    Add the map

--- src/component/Map.js
+++ src/component/Map.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import ReactMapGl, { Marker } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "mapbox-gl";
@@ -7,7 +7,9 @@ import "./map.css";
 // mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 export default function Map() {
-  const [selectedLanguage, setSelectedLanguage] = useState(null);
+  const mapRef = useRef();
+  const [leftDisplay, setLeftDisplay] = useState(false);
+  const [rightDisplay, setRightDisplay] = useState(false);
   const REACT_APP_MAPBOX_TOKEN =
     "pk.eyJ1IjoiZ3VuZXJpYm9pIiwiYSI6ImNrMnM0NjJ1dzB3cHAzbXVpaXhrdGd1YjIifQ.1TmNd7MjX3AhHdXprT4Wjg";
   const [viewpoint, setViewpoint] = useState({
@@ -17,44 +19,54 @@ export default function Map() {
     height: "100vh",
     zoom: 3.7,
   });
+  function toggleSidebar() {}
   return (
     <div className="body">
       <div id="map">
         <ReactMapGl
+          ref={mapRef}
           {...viewpoint}
           mapboxApiAccessToken={REACT_APP_MAPBOX_TOKEN}
           onViewportChange={viewpoint => {
             setViewpoint(viewpoint);
           }}
           mapStyle="mapbox://styles/guneriboi/ckliz10u80f7817mtlpnik90t"
-        ></ReactMapGl>
-      </div>
-      <div id="logo">
-        <img
-          src="https://vs360maiwar.s3-ap-southeast-2.amazonaws.com/img/logo.svg"
-          alt="LOGO"
-        />
-        <h1>VIRTUAL SONGLINES</h1>
-      </div>
-      <div id="left" class="sidebar flex-center left collapsed">
-        <div class="sidebar-content rounded-rect flex-center">
-          <div id="listings" class="listings"></div>
-
-          <div
-            class="sidebar-toggle rounded-rect left"
-            onclick="toggleSidebar('left')"
-          >
-            <p>Projects</p>
+          onClick={e => {
+            const features = mapRef.current.queryRenderedFeatures(e.point, {
+              layers: ["vs-info"],
+            });
+            var feature = features[0];
+            feature && console.log(feature.properties.VideoName);
+          }}
+        >
+          <div id="logo">
+            <img
+              src="https://vs360maiwar.s3-ap-southeast-2.amazonaws.com/img/logo.svg"
+              alt="LOGO"
+            />
+            <h1>VIRTUAL SONGLINES</h1>
           </div>
-        </div>
-      </div>
+          <div id="left" class="sidebar flex-center left collapsed">
+            <div class="sidebar-content rounded-rect flex-center">
+              <div id="listings" class="listings"></div>
 
-      <div id="right" class="sidebar flex-center right collapsed">
-        <div class="sidebar-content rounded-rect flex-center">
-          <div>
-            <pre id="features"></pre>
+              <div
+                class="sidebar-toggle rounded-rect left"
+                onclick="toggleSidebar('left')"
+              >
+                <p>Projects</p>
+              </div>
+            </div>
           </div>
-        </div>
+
+          <div id="right" class="sidebar flex-center right collapsed">
+            <div class="sidebar-content rounded-rect flex-center">
+              <div>
+                <pre id="features"></pre>
+              </div>
+            </div>
+          </div>
+        </ReactMapGl>
       </div>
     </div>
   );
