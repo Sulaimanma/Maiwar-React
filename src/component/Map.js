ed7c7e3de3c0878d62d4efa1e68b8d2ec208cb4d
Sulaimanma <379043678@qq.com>
2021-05-04 13:14:09 +1000

    Changed the pitch

--- src/component/Map.js
+++ src/component/Map.js
@@ -4,29 +4,33 @@ import React, {
   useEffect,
   useRef,
   useState,
-} from "react"
-import ReactMapGl, { Marker, Popup, Source, Layer } from "react-map-gl"
-import "mapbox-gl/dist/mapbox-gl.css"
-import mapboxgl from "mapbox-gl"
-import "./map.css"
-import { clusterLayer, clusterCountLayer, unclusteredPointLayer } from "./layer"
+} from "react";
+import ReactMapGl, { Marker, Popup, Source, Layer } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import mapboxgl from "mapbox-gl";
+import "./map.css";
+import {
+  clusterLayer,
+  clusterCountLayer,
+  unclusteredPointLayer,
+} from "./layer";
 
-import PopInfo from "./PopInfo"
-import Pins from "./Pins"
-import Sidebar from "./Sidebar/Sidebar"
-import DragPin from "./DragPin"
+import PopInfo from "./PopInfo";
+import Pins from "./Pins";
+import Sidebar from "./Sidebar/Sidebar";
+import DragPin from "./DragPin";
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 
 export default function Map() {
-  const mapRef = useRef()
+  const mapRef = useRef();
   // popup control variable
 
-  const [popup, setPopup] = useState(false)
+  const [popup, setPopup] = useState(false);
   // mapbox Token
   const REACT_APP_MAPBOX_TOKEN =
-    "pk.eyJ1IjoiZ3VuZXJpYm9pIiwiYSI6ImNrMnM0NjJ1dzB3cHAzbXVpaXhrdGd1YjIifQ.1TmNd7MjX3AhHdXprT4Wjg"
+    "pk.eyJ1IjoiZ3VuZXJpYm9pIiwiYSI6ImNrMnM0NjJ1dzB3cHAzbXVpaXhrdGd1YjIifQ.1TmNd7MjX3AhHdXprT4Wjg";
   //Initial Viewpoint
   const [viewpoint, setViewpoint] = useState({
     latitude: -27.477173,
@@ -35,88 +39,88 @@ export default function Map() {
     height: "100vh",
     zoom: 4.5,
     bearing: 0,
-    pitch: 30,
-  })
+    pitch: 0,
+  });
   //Fetched data
-  const [allData, setAllData] = useState(null)
+  const [allData, setAllData] = useState(null);
   //Fetch resource to detect whether it is valid or not
-  const [resource, setResource] = useState(null)
+  const [resource, setResource] = useState(null);
   //Data for display
-  const [clickInfo, setclickInfo] = useState(null)
+  const [clickInfo, setclickInfo] = useState(null);
   // Fetch the Layer GeoJson data for display
   useEffect(() => {
     /* global fetch */
     fetch(
       "https://amplifylanguageappgidarjil114226-dev.s3-ap-southeast-2.amazonaws.com/public/wordlist/features.geojson"
     )
-      .then((res) => res.json())
-      .then((json) => setAllData(json))
-  }, [])
+      .then(res => res.json())
+      .then(json => setAllData(json));
+  }, []);
 
-  const onClick = useCallback((event) => {
+  const onClick = useCallback(event => {
     // Destructure features from the click event data
-    const { features } = event
+    const { features } = event;
     // Make sure feature data is not undefined
-    const clickedFeature = features && features[0]
+    const clickedFeature = features && features[0];
     //Control the state of pop up
-    setPopup(true)
+    setPopup(true);
     //Set the data to display
-    setclickInfo(clickedFeature)
-  }, [])
+    setclickInfo(clickedFeature);
+  }, []);
 
   //Video function to play the video according to the Video Name
-  const video = (VideoName) => {
-    console.log(VideoName)
+  const video = VideoName => {
+    console.log(VideoName);
     if (VideoName === "Ducks" || VideoName === "Gathering Bush") {
       var path =
-        "https://vs360maiwar.s3-ap-southeast-2.amazonaws.com/video/Camp_Bush_Children_Running.mp4"
+        "https://vs360maiwar.s3-ap-southeast-2.amazonaws.com/video/Camp_Bush_Children_Running.mp4";
     } else {
       var path =
         "https://vs360maiwar.s3-ap-southeast-2.amazonaws.com/video/" +
         VideoName +
-        ".mp4"
+        ".mp4";
     }
     // Make sure each Url is valid
 
-    return path
-  }
+    return path;
+  };
 
   //Initial the marker position
   const [marker, setMarker] = useState({
     latitude: -27.477173,
     longitude: 138.014308,
-  })
+  });
   //Initial the events
-  const [events, logEvents] = useState({})
-  const onMarkerDragStart = useCallback((event) => {
-    logEvents((_events) => ({ ..._events, onDragStart: event.lngLat }))
-  }, [])
+  const [events, logEvents] = useState({});
+  const onMarkerDragStart = useCallback(event => {
+    logEvents(_events => ({ ..._events, onDragStart: event.lngLat }));
+  }, []);
   // Detect the drag event
-  const onMarkerDrag = useCallback((event) => {
-    logEvents((_events) => ({ ..._events, onDrag: event.lngLat }))
-  }, [])
+  const onMarkerDrag = useCallback(event => {
+    logEvents(_events => ({ ..._events, onDrag: event.lngLat }));
+  }, []);
 
-  const onMarkerDragEnd = useCallback((event) => {
-    logEvents((_events) => ({ ..._events, onDragEnd: event.lngLat }))
+  const onMarkerDragEnd = useCallback(event => {
+    logEvents(_events => ({ ..._events, onDragEnd: event.lngLat }));
     setMarker({
       longitude: event.lngLat[0],
       latitude: event.lngLat[1],
-    })
-  }, [])
+    });
+  }, []);
   // locate the user label on the map
   const locateUser = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
+    navigator.geolocation.getCurrentPosition(position => {
       setMarker({
         longitude: position.coords.longitude,
         latitude: position.coords.latitude,
-      })
+      });
       setViewpoint({
         ...viewpoint,
         longitude: position.coords.longitude,
         latitude: position.coords.latitude,
-      })
-    })
-  }
+      });
+    });
+  };
   // const label = (e) => {
   //   const locationData = mapRef.current.queryRenderedFeatures(e.point, {})
   //   // const geoData = locationData[0].geometry.coordinates[0][0][0];
@@ -126,7 +130,7 @@ export default function Map() {
   // geoLocation
   // console.log("events", events.onDrag)
 
-  console.log(allData)
+  console.log(allData);
   return (
     <div className="body" id="body">
       <Sidebar
@@ -139,8 +143,8 @@ export default function Map() {
           ref={mapRef}
           {...viewpoint}
           mapboxApiAccessToken={REACT_APP_MAPBOX_TOKEN}
-          onViewportChange={(viewpoint) => {
-            setViewpoint(viewpoint)
+          onViewportChange={viewpoint => {
+            setViewpoint(viewpoint);
           }}
           mapStyle="mapbox://styles/guneriboi/cko26dcwb028v17qgn1swhijn"
           //Define the interactive layer
@@ -203,7 +207,7 @@ export default function Map() {
         </ReactMapGl>
       </div>
     </div>
-  )
+  );
 }
 
 // const onClickFun = e => {
